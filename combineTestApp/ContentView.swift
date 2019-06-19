3c254e455503f18f4e3c1766ba7fc5542f86780a
Allison McEntire <amcentire@deloitte.com>
2019-06-19 12:47:59 -0700

    Initial Commit

---
+++ combineTestApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  combineTestApp
+//
+//  Created by MacOS Test Account on 6/19/19.
+//  Copyright © 2019 MacOS Test Account. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
